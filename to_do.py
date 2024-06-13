93fdc529aab4d59dbe8334871cdfb0ee806f05bb
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-06-13 19:48:06 +0300

    add class user

--- to_do.py
+++ to_do.py
@@ -3,3 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    tasks = relationship("Task", backref="user")
